a101d703ca5c9830737d12a158779b93ca3e7e13
FranciscoPS <57423154+FranciscoPS@users.noreply.github.com>
2023-06-14 08:37:33 -0600

    5. Componentes

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,17 @@
 <script>
+  import About from "./components/about.svelte";
+
   //Los export van a servir para obtener los valores de props, en este caso el name y lastName del main.js
   export let name;
   export let lastName;
   //Variables que no requieren ser obtenidas de props y se mantienen en local del archivo
   const svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
-  const gitHubUrl = "https://github.com/FranciscoPS";
 </script>
 
 <main>
   <h1>Hello {name} {lastName} !</h1>
-  <p>
-    A <a href={gitHubUrl} target="_blank">Francisco Peralta's</a> project.
-  </p>
+
+  <About></About>
 
   <img src={svelteLogo} alt="Svelte Logo" />
 </main>
@@ -27,11 +27,6 @@
     --theme-color: purple;
   }
 
-  p {
-    color: var(--theme-color);
-
-    font-size: 22px;
-  }
   /* End of Global Styles */
   main {
     text-align: center;
---
+++ src/components/about.svelte
@@ -0,0 +1,17 @@
+<script>
+  const gitHubUrl = "https://github.com/FranciscoPS";
+</script>
+
+<section>
+  <p>
+    A <a href={gitHubUrl} target="_blank">Francisco Peralta</a> project.
+  </p>
+</section>
+
+<style>
+  p {
+    color: var(--theme-color);
+
+    font-size: 22px;
+  }
+</style>
