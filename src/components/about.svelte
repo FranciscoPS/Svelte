6dcacfa73d5b4507901958e365a30812679883bb
FranciscoPS <57423154+FranciscoPS@users.noreply.github.com>
2023-06-14 09:10:33 -0600

    6. Reactividad

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -1,11 +1,19 @@
 <script>
   const gitHubUrl = "https://github.com/FranciscoPS";
+
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
 </script>
 
 <section>
   <p>
     A <a href={gitHubUrl} target="_blank">Francisco Peralta</a> project.
   </p>
+
+  <button on:click={handleClick}>Click {count === 0 ? "" : count}</button>
 </section>
 
 <style>
